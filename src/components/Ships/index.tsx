966addc2a54cdae6abbd586f2099d6925cb30715
Amber <enigmalea@gmail.com>
2022-09-26 18:10:26 -0400

    feat: add lightGallery to ships page. added lightGallery package. removed docusaruaurs cards rom ships page.
    closes issue #6

--- src/components/Ships/index.tsx
+++ src/components/Ships/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import clsx from "clsx";
-import Link from "@docusaurus/Link";
+import LightGallery from "lightgallery/react";
 import styles from "./styles.module.css";
+
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
 
 type ShipsItem = {
 	title: string;
@@ -37,51 +44,94 @@ const ShipsList: ShipsItem[] = [
 	},
 ];
 
-function Ship({ title, ao3_tag, img, details }: ShipsItem) {
-	return (
-		<div className="card">
-			<div className="card__image">
-				<img className={styles.preview} src={img} />
-			</div>
-			<div className="card__body">
-				<h4>{title}</h4>
-				{/* <small>{details}</small> */}
-			</div>
-			{/* <div className="card__footer">
-					<Link
-						className={clsx(
-							"button button--secondary button--block ao3",
-							styles.nounderline,
-							styles.responsive
-						)}
-						to={ao3_tag}
-					>
-						tag
-					</Link>
-				</div> */}
-		</div>
-	);
-}
+// function Ship({ title, ao3_tag, img, details }: ShipsItem) {
+// 	return (
+// 		<div className="card">
+// 			<div className="card__image">
+// 				<LightGallery  plugins={[lgMediumZoom]}>
+// 					<figure
+// 						lg-background-color="white"
+// 						className="blog-images"
+// 						data-src={img}
+// 						data-lg-size="1600-1126"
+// 					>
+// 						<img src={img} />
+// 					</figure>
+// 				</LightGallery>
+// 			</div>
+// 			<div className="card__body">
+// 				<h4>{title}</h4>
+// 				{/* <small>{details}</small> */}
+// 			</div>
+// 			{/* <div className="card__footer">
+// 					<Link
+// 						className={clsx(
+// 							"button button--secondary button--block ao3",
+// 							styles.nounderline,
+// 							styles.responsive
+// 						)}
+// 						to={ao3_tag}
+// 					>
+// 						tag
+// 					</Link>
+// 				</div> */}
+// 		</div>
+// 	);
+// }
 
 export default function Ships(): JSX.Element {
 	return (
-		<section>
-			<p>Below are the ships I've purchased from{" "}
-			<a href="https://robinboob.herokuapp.com">Robinboob</a>. They're mine and
-			only mine*!
+		<div className={styles.ship}>
+			<p>
+				Below are the ships I've purchased from{" "}
+				<a href="https://robinboob.herokuapp.com">Robinboob</a>. They're mine
+				and only mine*!
 			</p>
 			<p>
 				<small>
-					*This is a joke, but robinboob isn't. Check it out to get your own ship
-					certificates and support an effort to make the internet a better place
-					for fandom.
+					*This is a joke, but robinboob isn't. Check it out to get your own
+					ship certificates and support an effort to make the internet a better
+					place for fandom.
 				</small>
 			</p>
-			<div className={styles.ships}>
-				{ShipsList.map((props, idx) => (
-					<Ship key={idx} {...props} />
-				))}
-			</div>
-		</section>
+			<LightGallery
+				speed={500}
+				download={false}
+				alignThumbnails="middle"
+				plugins={[lgThumbnail, lgZoom]}
+			>
+				<a
+					href="/img/ships/thebois.png"
+					data-sub-html="<h4>Dragon Age</h4><h3>The Bois</h3><em>art by <a href='https://thefoxinboots.tumblr.com'>thefoxinboots</a></em>"
+				>
+					<img
+						alt="Dragon Age: The Bois"
+						src="/img/ships/thebois.png"
+						className={styles.preview}
+					/>
+				</a>
+				<a href="/img/ships/ironlion.png" data-sub-html="<h4>Dragon Age</h4><h3>Iron Lion</h3>">
+					<img
+						alt="Dragon Age: Iron Lion"
+						src="/img/ships/ironlion.png"
+						className={styles.preview}
+					/>
+				</a>
+				<a href="/img/ships/brunoabacchio.png" data-sub-html="<h4>Jojo's Bizarre Adventures</h4><h3>Bruno/Abacchio</h3>">
+					<img
+						alt="Jojo's Bizarre Adventures: Bruno/Abacchio"
+						src="/img/ships/brunoabacchio.png"
+						className={styles.preview}
+					/>
+				</a>
+				<a href="/img/ships/zukka.png" data-sub-html="<h4>Avatar: The Last Airbender</h4><h3>Zukka</h3>">
+					<img
+						alt="Avatar: The Last Airbender: Zukka"
+						src="/img/ships/zukka.png"
+						className={styles.preview}
+					/>
+				</a>
+			</LightGallery>
+		</div>
 	);
 }
