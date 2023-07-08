9fe7a3b5a68bf3c16ae40598233c85f73afc4d5f
enigmalea <enigmalea@gmail.com>
2023-07-08 15:19:19 -0400

    updates landing page text and social media links.

--- src/components/Socials/index.tsx
+++ src/components/Socials/index.tsx
@@ -2,18 +2,25 @@ import {
   SiArchiveofourown,
   SiGithub,
   SiMastodon,
-  SiMyanimelist,
   SiTumblr,
 } from "react-icons/si";
 
+import { HiMiniRss } from "react-icons/hi2";
 import React from "react";
 import styles from "./styles.module.css";
 
 export default function Socials(): JSX.Element {
   return (
     <div className={styles.main}>
-      <h2>Socials</h2>
+      <h2>Follow Me</h2>
       <div className={styles.socials}>
+        <a
+          href="https://zapier.com/engine/rss/4727282/enigmalea"
+          title="RSS"
+          className={styles.socialButton}
+        >
+          <HiMiniRss />
+        </a>
         <a
           href="https://ao3.org/users/enigmalea/profile"
           title="AO3"
